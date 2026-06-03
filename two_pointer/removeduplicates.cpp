f0193e325198e7f8176710ba3e9d3f471079b1ae
tanuja0708 <tanujapandey398@gmail.com>
2026-06-03 19:21:58 +0530

    in list

---
+++ two_pointer/removeduplicates.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode{
+    int val ;
+    ListNode* next;
+
+    ListNode(int x){
+        val=x;
+        next=NULL;
+    }
+};
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+       ListNode* curr= head;
+       while(curr!=NULL && curr->next!=NULL){
+           if(curr->val==curr->next->val){
+               curr->next=curr->next->next;
+            }else{
+               curr=curr->next;
+            }
+       }
+       return head;
+    }
+}; 
