db8478ae645d27eaf75f7f3e88caaa18c6329067
tanuja0708 <tanujapandey398@gmail.com>
2026-06-11 16:39:42 +0530

    hasCycle

---
+++ fast_and_slow_pointers/01_LinkedListCycle.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+bool hasCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast!=NULL && fast->next!=NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    int main() {
+    ListNode* n1 = new ListNode(3);
+    ListNode* n2 = new ListNode(2);
+    ListNode* n3 = new ListNode(0);
+    ListNode* n4 = new ListNode(-4);
+
+    n1->next = n2;
+    n2->next = n3;
+    n3->next = n4;
+    n4->next = n2;   
+
+    cout << hasCycle(n1);  
+}
