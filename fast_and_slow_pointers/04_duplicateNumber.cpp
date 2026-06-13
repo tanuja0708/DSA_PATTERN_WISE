a74470f2593a53fae388d45aaf5151a7da1c6597
tanuja0708 <tanujapandey398@gmail.com>
2026-06-13 18:02:18 +0530

    updated

--- fast_and_slow_pointers/04_duplicateNumber.cpp
+++ fast_and_slow_pointers/04_duplicateNumber.cpp
@@ -1,12 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
-
 int findDuplicate(vector<int>& nums) {
         int slow=0,fast=0;
         while(true){
@@ -26,9 +20,14 @@ int findDuplicate(vector<int>& nums) {
     }
 
     int main() {
-    vector<int> nums = {1, 3, 4, 2, 2};
+        int n;
+        cin>>n;
+        vector<int> nums(n);
+        for(int i=0;i<n;i++){
+            cin>>nums[i];
+        }
 
-    cout << findDuplicate(nums) << endl;
+        cout << findDuplicate(nums) << endl;
 
-    return 0;
+        return 0;
 }
